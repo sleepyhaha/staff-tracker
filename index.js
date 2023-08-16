1cf2372f4f93ff5b89c9769734642b061b671f8a
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-16 22:56:37 +1000

    query edits

--- index.js
+++ index.js
@@ -26,29 +26,3 @@ const questions = [
     ],
   },
 ];
-
-const viewEmployees = () => {
-  db.query(
-    `SELECT employee.id, 
-        employee.first_name AS FirstName,
-        employee.last_name AS LastName,
-        role.title AS job,
-        department.name AS department,
-        role.salary AS salary,
-        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-        FROM employee
-        JOIN role
-        ON employee.role_id = role.id
-        JOIN department
-        ON role.department_id = department.id
-        LEFT JOIN employee
-        AS manager
-        ON employee.manager_id = manager.id`,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(result);
-    }
-  );
-};
