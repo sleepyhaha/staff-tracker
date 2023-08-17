9d94e8d252e83c6faa06c38ca4b2ceb8861fd98d
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-17 21:49:30 +1000

    finished adding inquirer prompts

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection(
   console.log("Connected to staffDB")
 );
 
-const questions = [
+const mainMenu = [
   {
     type: "list",
     message: "What would you like to do?",
@@ -45,5 +45,54 @@ const addDepartmentPrompt = async () => {
     addDepartment(data);
   } catch (err) {
     console.log("There was an error");
+  }
+};
+
+const addRolePrompt = async () => {
+  try {
+    await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the role title?",
+        name: "roleTitle",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter a role title.");
+          }
+          return false;
+        },
+      },
+      {
+        type: "input",
+        message: "What is the salary for the role?",
+        name: "roleSalary",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter a salary");
+          }
+          return false;
+        },
+      },
+      {
+        type: "input",
+        message: "What department is the role in?",
+        name: "roleDept",
+        validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            console.log("Please enter a department");
+          }
+          return false;
+        },
+      },
+    ]);
+    addRole(data);
+  } catch (err) {
+    console.log("There was an error.");
   }
 };
